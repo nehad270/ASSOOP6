4baccbae597a6ad430fd13e1678f9ba0b8d86fc5
nehad <Dark@DESKTOP-TQDVAO4>
2025-08-06 04:57:03 -0700

    First Project:

---
+++ Point3D.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASSOOP6
+{
+    internal class Point3D : IComparable<Point3D>, ICloneable
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+        public Point3D() : this(0, 0, 0) { }
+
+        public Point3D(int value) : this(value, value, value) { }
+
+        public Point3D(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public override string ToString()
+        {
+            return $"Point Coordinates: ({X}, {Y}, {Z})";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Point3D other)
+                return X == other.X && Y == other.Y && Z == other.Z;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y, Z);
+        }
+
+        public static bool operator ==(Point3D p1, Point3D p2)
+        {
+            return Equals(p1, p2);
+        }
+
+        public static bool operator !=(Point3D p1, Point3D p2)
+        {
+            return !Equals(p1, p2);
+        }
+
+        public int CompareTo(Point3D other)
+        {
+            if (X != other.X)
+                return X.CompareTo(other.X);
+            return Y.CompareTo(other.Y);
+        }
+
+        public object Clone()
+        {
+            return new Point3D(X, Y, Z);
+        }
+
+      
+    }
+}
